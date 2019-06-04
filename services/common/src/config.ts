509c3c6b6fcb2102a556e2546d2fa71e844c3c61
Jeremy Chone <jeremy.chone@gmail.com>
2019-06-03 19:14:47 -0700

    . minor update, add aws to vdev.yalm

--- services/common/src/config.ts
+++ services/common/src/config.ts
@@ -2,7 +2,6 @@ import { getKnex } from './da/db';
 
 // IMPORTANT: Do not change this appVersion value manually, change it in the package.json and do a "npm run version"
 const staticConfigurations: any = {
-	dataPath: "/service/data",
 	appVersion: "DROP-001-SNAPSHOT"
 }
 
