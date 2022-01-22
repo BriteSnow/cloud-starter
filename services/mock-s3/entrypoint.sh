bfef53afc61c12698507466ee59b7204f5e8aa9f
Jeremy Chone <jeremy.chone@gmail.com>
2022-01-22 10:44:00 -0800

    . mock-s3 - update docker & k8s to create dev bucket a apply time

---
+++ services/mock-s3/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start the minio server as background
+minio server /data --console-address :9900 & disown
+
+# Cheap way to wait ath the server is started (since we start it as background)
+sleep 3s
+
+# Create the minio alias pointing to this server
+mc alias set minio http://localhost:9000 minio miniominio --api S3v4
+
+# ----
+# Seeding the buckets for dev and set them download (no sign in minio/dev environment)
+# Prod will use aws s3 buckets
+mc mb minio/core-bucket
+mc policy set download minio/core-bucket
+
+mc mb minio/logs-bucket
+mc policy set download minio/logs-bucket
+
+
+# wait forever
+tail -f /dev/null
